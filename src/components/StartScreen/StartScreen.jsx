8c8c565da66d6964749bd794a2c2930706caf2df
Achette <i.achette@live.com>
2022-06-13 11:55:14 -0300

    First commit

---
+++ src/components/StartScreen/StartScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+        <h1>SECRET WORD</h1>
+        <p>Clique no botão abaixo para começar a jogar</p>
+        <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen

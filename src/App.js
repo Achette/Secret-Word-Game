76a63a4eb2f52864582c074ae015521e1d56fdbe
Achette <i.achette@live.com>
2022-06-13 12:17:28 -0300

    Created game components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+// IMPORTS DO REACT
+import { useCallback, useEffect, useState } from "react";
 
-import './App.css';
-import StartScreen from './components/StartScreen/StartScreen';
+// CSS
+import "./App.css";
+
+// DATA
+import { wordsList } from "./data/words";
+
+// COMPONENTS
+import StartScreen from "./components/StartScreen/StartScreen";
+import Game from "./components/Game/Game";
+import GameOver from "./components/GameOver/GameOver";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
   return (
     <div className="App">
-     <StartScreen />
+     {gameStage === 'start' &&  <StartScreen startGame={startGame} />}
+     {gameStage === 'game' &&  <Game />}
+     {gameStage === 'end' &&  <GameOver />}
     </div>
   );
 }
